c82816f9be6b40ca5ae2c49f6e6a827b186153bf
Connor <cqm@andrew.cmu.edu>
2016-03-30 15:31:57 -0400

    added ubuntu startup script

---
+++ startup.sh
@@ -0,0 +1,60 @@
+sudo apt-get update
+
+sudo apt-get install vim
+
+# Git config
+sudo apt-get install git
+git config --global user.name "Connor"
+git config --global user.email cqm@andrew.cmu.edu
+git config --global core.editor vim
+
+# Install sublime 3
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+sudo apt-get update
+sudo apt-get install sublime-text-installer
+
+# Install terminator
+sudo add-apt-repository ppa:gnome-terminator
+sudo apt-get update
+sudo apt-get install terminator
+
+# Get dotfiles
+cd ~
+git clone https://github.com/connorqmoore/dotfiles.git
+cd dotfiles
+mv .vimperatorrc ..
+mv .vimrc ..
+cd ..
+rm -rf dotfiles
+
+# Setup Vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
+git clone https://github.com/ghuntley/terminator-solarized.git
+cd terminator-solarized
+mkdir -p ~/.config/terminator/
+touch ~/.config/terminator/config
+# if you want to replace current config:
+cp config ~/.config/terminator
+# if you want to append current config:
+cat config >> ~/.config/terminator/config
+
+cd ..
+rm -rf terminator-solarized
+
+# Setup workspaces -> type in workspaces and change that to 4,
+#             next right click on the section where you want to add 
+#             the workspaces and add them in
+
+# Add vimperator
+
+# Setup Ruby
+apt-get install curl
+command curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+\curl -sSL https://get.rvm.io | bash -s stable
+rvm install 2.2.0
+apt-get install nodejs
+rvm use 2.2.0
+gem install rails -v=4.0.13
+
